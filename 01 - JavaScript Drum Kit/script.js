fa1e96634b3bb8e618eee48473abeb5d4b394b55
Lukas Jerusalem <lukasjerusalem@web.de>
2025-07-01 23:16:23 +0200

    Apply class playing on pressed key

--- 01 - JavaScript Drum Kit/script.js
+++ 01 - JavaScript Drum Kit/script.js
@@ -1,8 +1,47 @@
 const keys = document.querySelectorAll(".key");
 let keysArray = Array.from(keys);
-const clap = document.getElementById("Clap");
+
+const clap = keysArray[0];
+const hihat = keysArray[1];
+const kick = keysArray[2];
+const openhat = keysArray[3];
+const boom = keysArray[4];
+const ride = keysArray[5];
+const snare = keysArray[6];
+const tom = keysArray[7];
+const tink = keysArray[8];
 
 document.addEventListener("keydown", function(event) {
-  const key = event.key;
-  console.log(key);
+  const key = event.key.toUpperCase();
+  switch(key) {
+    case "A":
+      clap.classList.add("playing");
+      break;
+    case "S":
+      hihat.classList.add("playing");
+      break;
+    case "D":
+      kick.classList.add("playing");
+      break;
+    case "F":
+      openhat.classList.add("playing");
+      break;
+    case "G":
+      boom.classList.add("playing");
+      break;
+    case "H":
+      ride.classList.add("playing");
+      break;
+    case "J":
+      snare.classList.add("playing");
+      break;
+    case "K":
+      tom.classList.add("playing");
+      break;
+    case "L":
+      tink.classList.add("playing");
+      break;
+    default:
+      break;
+  }
 })
