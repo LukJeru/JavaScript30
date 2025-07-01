b1132d41d499e8ff35393263b18d361477dfd697
Lukas Jerusalem <lukasjerusalem@web.de>
2025-07-01 23:02:57 +0200

    Add event listener on document

--- 01 - JavaScript Drum Kit/script.js
+++ 01 - JavaScript Drum Kit/script.js
@@ -1,3 +1,8 @@
 const keys = document.querySelectorAll(".key");
 let keysArray = Array.from(keys);
 const clap = document.getElementById("Clap");
+
+document.addEventListener("keydown", function(event) {
+  const key = event.key;
+  console.log(key);
+})
