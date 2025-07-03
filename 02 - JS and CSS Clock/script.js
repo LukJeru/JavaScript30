e1e9f75355d4c885454afe0ad5dfba8c16627ac6
Lukas Jerusalem <lukasjerusalem@web.de>
2025-07-03 21:26:17 +0200

    Add two functions to get the current time

--- 02 - JS and CSS Clock/script.js
+++ 02 - JS and CSS Clock/script.js
@@ -4,28 +4,30 @@ let hours = currentDate.getHours();
 let minutes = currentDate.getMinutes();
 let seconds = currentDate.getSeconds();
 
+//select hands
 const hourHand = document.querySelector('.hour-hand');
-const minutesHand = document.querySelector('.min-hand');
+const minuteHand = document.querySelector('.min-hand');
 const secondHand = document.querySelector('.second-hand');
 
+//get degrees
 const hourDegree = 360/24;
 const minuteDegree = 360/60;
 const secondDegree = 360/60;
 
+//set hands on zero
+hourHand.style.transform = "rotate(90deg)";
+minuteHand.style.transform = "rotate(90deg)";
+secondHand.style.transform = "rotate(90deg)";
+
 function showTime() {
-  //set all hands on zero
-  hourHand.style.transform = "rotate(90deg)";
-  minutesHand.style.transform = "rotate(90deg)";
-  secondHand.style.transform = "rotate(90deg)";
+  //set hands on current time
+  hourHand.style.transform = buildRotationString(hourDegree, hours);
+  minuteHand.style.transform = buildRotationString(minuteDegree, minutes);
+  secondHand.style.transform = buildRotationString(secondDegree, seconds);
+}
+
+function buildRotationString(degree, number) {
+  return `rotate(${degree * number}deg)`;
 }
 
 showTime();
-//infinity loop to update time
-/*
-while (true){
-  currentDate = new Date();
-  hours = currentDate.getHours();
-  minutes = currentDate.getMinutes();
-  seconds = currentDate.getSeconds();
-};
-*/
