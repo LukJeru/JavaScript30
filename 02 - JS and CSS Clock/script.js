0d7f4c2d0affad470a1c39ab64e0fec9a3db543f
Lukas Jerusalem <lukasjerusalem@web.de>
2025-07-03 21:16:01 +0200

    Set all hands on zero

--- 02 - JS and CSS Clock/script.js
+++ 02 - JS and CSS Clock/script.js
@@ -1,1 +1,31 @@
+//get current date and declare variables for each time element with global scope
+let currentDate = new Date();
+let hours = currentDate.getHours();
+let minutes = currentDate.getMinutes();
+let seconds = currentDate.getSeconds();
 
+const hourHand = document.querySelector('.hour-hand');
+const minutesHand = document.querySelector('.min-hand');
+const secondHand = document.querySelector('.second-hand');
+
+const hourDegree = 360/24;
+const minuteDegree = 360/60;
+const secondDegree = 360/60;
+
+function showTime() {
+  //set all hands on zero
+  hourHand.style.transform = "rotate(90deg)";
+  minutesHand.style.transform = "rotate(90deg)";
+  secondHand.style.transform = "rotate(90deg)";
+}
+
+showTime();
+//infinity loop to update time
+/*
+while (true){
+  currentDate = new Date();
+  hours = currentDate.getHours();
+  minutes = currentDate.getMinutes();
+  seconds = currentDate.getSeconds();
+};
+*/
